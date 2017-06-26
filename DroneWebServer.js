c7476df8d168f4259985e8cb1fd0712f5abf3efc
jbart10 <jwbart10@gmail.com>
2017-06-26 12:53:15 -0700

    Update DroneWebServer.js

--- DroneWebServer.js
+++ DroneWebServer.js
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 // to take off
 app.get('/takeoff', function(req, res) {
   client.takeoff();
-  console.log("Barth Joe Taking Off");
+  console.log("BarthJoe Taking Off");
 });
 
 // This router is sending a command to the drone
